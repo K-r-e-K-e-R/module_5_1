f434931e4cdd8467cdb2a204a1c2a95dfbd7acc3
K-r-e-K-e-R <san.volga034@bk.ru>
2024-09-27 02:07:19 +0500

    Update module_5_1.py

--- module_5_1.py
+++ module_5_1.py
@@ -9,6 +9,16 @@ class House:
         else:
             print("Такого этажа не существует.")
 
+    def total_floors_first(self):
+        print(f"Вы поднимайтесь на этаж здания {self.name}")
+        for i in range(1, 8):
+            print(i)
+
+    def total_floors_second(self):
+        print(f"Вы поднимайтесь на этаж здания {self.name}")
+        for j in range(1, 12):
+            print(j)
+
 
 home1 = House("ЖК Эльбрус", 10)
 home2 = House("ЖК Альбатрос", 16)
@@ -16,6 +26,9 @@ home2 = House("ЖК Альбатрос", 16)
 print(home1.name, home1.floors)
 print(home2.name, home2.floors)
 
-home1.go_to(5)
+home1.total_floors_first()
+home2.total_floors_second()
+
+home1.go_to(8)
 home1.go_to(15)
-home2.go_to(2)
+home2.go_to(12)
