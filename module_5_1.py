39b1b62e58c9d550d2ca6a9df903fc95c0428324
K-r-e-K-e-R <san.volga034@bk.ru>
2024-10-20 23:26:25 +0500

    Update module_5_1.py
    Пересобран код, упрошено применение

--- module_5_1.py
+++ module_5_1.py
@@ -1,34 +1,22 @@
 class House:
-    def __init__(self, name, floors):
-        self.name = name
-        self.floors = floors
+    class House:
+        def __init__(self, name, floors):
+            self.name = name
+            self.floors = floors
 
-    def go_to(self, new_floor):
-        if 1 <= new_floor <= self.floors:
-            print(f"Вы находитесь на этаже {new_floor} в здании {self.name}.")
-        else:
-            print("Такого этажа не существует.")
+        def go_to(self, new_floor):
+            if 1 <= new_floor <= self.floors:
+                print(f"Вы поднимаетесь на этаж {new_floor} в здании {self.name}.")
+                for i in range(1, new_floor + 1):
+                    print(f"Этаж {i}")
+            else:
+                print("Такого этажа не существует.")
 
-    def total_floors_first(self):
-        print(f"Вы поднимайтесь на этаж здания {self.name}")
-        for i in range(1, 8):
-            print(i)
+    home1 = House("ЖК Эльбрус", 10)
+    home2 = House("ЖК Альбатрос", 16)
 
-    def total_floors_second(self):
-        print(f"Вы поднимайтесь на этаж здания {self.name}")
-        for j in range(1, 12):
-            print(j)
+    print(home1.name, home1.floors)
+    print(home2.name, home2.floors)
 
-
-home1 = House("ЖК Эльбрус", 10)
-home2 = House("ЖК Альбатрос", 16)
-
-print(home1.name, home1.floors)
-print(home2.name, home2.floors)
-
-home1.total_floors_first()
-home2.total_floors_second()
-
-home1.go_to(8)
-home1.go_to(15)
-home2.go_to(12)
+    home1.go_to(5)
+    home2.go_to(17)
